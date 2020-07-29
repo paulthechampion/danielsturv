de5c9535e37ae0d75053d62da9cebed7a9ea7361
paulthechampion <enwerempaulo@gmail.com>
2020-07-29 03:29:43 -0400

    another

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const router = express.Router();
+const app = express();
 
 
-router.get('/', (req,res)=>{
+app.get('/', (req,res)=>{
     res.render("index");
 
 })
 
 
 
-module.exports = router;
+module.exports = app;
